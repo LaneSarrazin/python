67a4490ca869fbb6659302ad8ca01557f9cb5520
LaneSarrazin <rosimarreta@hotmail.com>
2023-09-12 20:42:12 -0300

    usando metodos

---
+++ moeda.py
@@ -0,0 +1,19 @@
+def Aumentar(preco, taxa):
+    res = preco + (preco*taxa/100)
+    return res
+
+def Diminuir(preco, taxa):
+    res = preco - (preco* taxa/100)
+    return res
+
+def Dobro(preco):
+    res = preco * 2
+    return res
+
+def Metade(preco):
+    res = preco / 2
+    return res
+
+
+
+

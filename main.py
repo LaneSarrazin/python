e516f8cfb5785c575c7f683036ebb27500658d7f
LaneSarrazin <rosimarreta@hotmail.com>
2023-09-12 20:27:13 -0300

    usando metodos

---
+++ main.py
@@ -0,0 +1,13 @@
+#import funcao
+from funcao import Somar, Multi
+
+#estou puxando o somar de funcao
+
+Somar()
+Multi()
+
+#crie um modulo chamado moeda.py que tenha as funcoes 
+#incoporadas aumentar(), diminuir(), dobro() e metade().
+#faça tambem um programa que importa esse modulo e use algumas 
+#dessas funcoes crie o modulo teste.py
+

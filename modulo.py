c5ff00c5c99c73cda4072cd0796c055e284fa716
LaneSarrazin <rosimarreta@hotmail.com>
2023-09-12 20:25:37 -0300

    usando metodos

---
+++ modulo.py
@@ -0,0 +1,16 @@
+#modulos 
+
+import math 
+
+num = int(input("digite um numero: "))
+
+raiz = math.sqrt(num)
+
+print('a raiz de {} e igual a {}'.format(num, raiz))
+
+#ele tira os numeros depois da virgula!
+print('a raiz de {} e igual a {}'.format(num, math.floor(raiz)))
+
+
+
+
